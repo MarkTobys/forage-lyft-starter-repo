633599a09c03bb2e72f3cd5a283f4cdbba6f4767
Mark Tobys <marktobys0@gmail.com>
2024-02-04 20:00:43 +1000

    lyft code (actual)

--- test/test_car.py
+++ test/test_car.py
@@ -1,11 +1,6 @@
 import unittest
 from datetime import datetime
-
-from engine.model.calliope import Calliope
-from engine.model.glissade import Glissade
-from engine.model.palindrome import Palindrome
-from engine.model.rorschach import Rorschach
-from engine.model.thovex import Thovex
+from car_factory import CarFactory
 
 
 class TestCalliope(unittest.TestCase):
@@ -14,9 +9,9 @@ class TestCalliope(unittest.TestCase):
         last_service_date = today.replace(year=today.year - 3)
         current_mileage = 0
         last_service_mileage = 0
-
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertTrue(car.needs_service())
+        car = CarFactory.create_calliope(today, last_service_date, current_mileage, last_service_mileage)
+        print(car.engine)
+        self.assertTrue(car.engine.needs_service() or car.battery.needs_service())
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
